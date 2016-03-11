34983dc7f117798288a32dc587dd297846294e35
Peter <stop_time@list.ru>
2016-03-12 00:30:14 +0200

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #sudo apt-get install nginx
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 #sudo /etc/init.d/nginx start
